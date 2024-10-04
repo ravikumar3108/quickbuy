9b9f9a63c281d383bdb732e6e1cce8d8e82f49b9
ravikumar <raviluhaniwal318@gmail.com>
2024-10-04 22:05:59 +0530

    frist upload

--- index.js
+++ index.js
@@ -14,10 +14,20 @@ const corsOptions = {
 }
 
 
-// main().catch(err => console.log(err));
-// async function main() {
-//     await mongoose.connect('mongodb://127.0.0.1:27017/test');
-// }
+main().catch(err => console.log(err));
+async function main() {
+    try {
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+        console.log(
+          "Database connected: ",
+          connect.connection.host,
+          connect.connection.name
+        );
+      } catch (err) {
+        console.log(err);
+        process.exit(1);
+      }
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
